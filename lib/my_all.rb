9432dcedab37b66621f67bf1aef1b85c95d095d7
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-15 16:52:14 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,17 +2,18 @@ require 'pry'
 
 def my_all?(collection)
   if block_given?
+    
     i = 0
     block_return_values = []
-    while i < collection.length
-      block_return_values << yield(collection[i])
-      i++
-    end
-    if block_return_values.include?(false)
-      false
-    else
-      true
-    end
+      while i < collection.length
+        block_return_values << yield(collection[i])
+        i++
+      end
+      if block_return_values.include?(false)
+        false
+      else
+        true
+      end
   else
     puts "No block given."
   end
