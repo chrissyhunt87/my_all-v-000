74a8de1d9c5d973893d33974fe9668886d03b996
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-15 16:48:45 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,9 +4,10 @@ def my_all?(collection)
   if block_given?
     i = 0
     while i < collection.length
-      
+      yield collection[i]
       i++
     end
+    collection
   else
     puts "No block given."
   end
